5dfb1bda7b37bd025e3dd868edc28580bf442190
BlueblueWay <BlueblueWay@users.noreply.github.com>
2022-09-19 14:11:01 -0700

    minor change

--- src/main/java/com/retailer/rewards/RewardPointsApplication.java
+++ src/main/java/com/retailer/rewards/RewardPointsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RewardsPointsApplication {
+public class RewardPointsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RewardsPointsApplication.class, args);
+        SpringApplication.run(RewardPointsApplication.class, args);
     }
 
 }
